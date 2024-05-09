c0e71d8ab44aa254102bc898336c44e3954c3bf5
akumax7 <danielgajardobroh@gmail.com>
2024-05-09 16:28:58 -0400

    Primer Commit

---
+++ storage/logs/log-2024-05-07.php
@@ -0,0 +1,28 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-05-07 00:22:31 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\easyappointments\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-05-07 00:22:31 --> Severity: error --> Exception: Unable to connect to the database. C:\xampp\htdocs\easyappointments\system\database\DB_driver.php 433
+ERROR - 2024-05-07 00:22:36 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\easyappointments\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-05-07 00:22:36 --> Severity: error --> Exception: Unable to connect to the database. C:\xampp\htdocs\easyappointments\system\database\DB_driver.php 433
+ERROR - 2024-05-07 00:22:40 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\easyappointments\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-05-07 00:22:40 --> Severity: error --> Exception: Unable to connect to the database. C:\xampp\htdocs\easyappointments\system\database\DB_driver.php 433
+ERROR - 2024-05-07 00:23:02 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\xampp\htdocs\easyappointments\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-05-07 00:23:02 --> Severity: error --> Exception: Unable to connect to the database. C:\xampp\htdocs\easyappointments\system\database\DB_driver.php 433
+ERROR - 2024-05-07 00:26:37 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\xampp\htdocs\easyappointments\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-05-07 00:26:37 --> Severity: error --> Exception: Unable to connect to the database. C:\xampp\htdocs\easyappointments\system\database\DB_driver.php 433
+ERROR - 2024-05-07 00:26:39 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\xampp\htdocs\easyappointments\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-05-07 00:26:39 --> Severity: error --> Exception: Unable to connect to the database. C:\xampp\htdocs\easyappointments\system\database\DB_driver.php 433
+ERROR - 2024-05-07 00:26:40 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\xampp\htdocs\easyappointments\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-05-07 00:26:40 --> Severity: error --> Exception: Unable to connect to the database. C:\xampp\htdocs\easyappointments\system\database\DB_driver.php 433
+ERROR - 2024-05-07 00:26:40 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\xampp\htdocs\easyappointments\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-05-07 00:26:40 --> Severity: error --> Exception: Unable to connect to the database. C:\xampp\htdocs\easyappointments\system\database\DB_driver.php 433
+ERROR - 2024-05-07 00:26:58 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\xampp\htdocs\easyappointments\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-05-07 00:26:58 --> Severity: error --> Exception: Unable to connect to the database. C:\xampp\htdocs\easyappointments\system\database\DB_driver.php 433
+ERROR - 2024-05-07 00:35:24 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\xampp\htdocs\easyappointments\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-05-07 00:35:24 --> Severity: error --> Exception: Unable to connect to the database. C:\xampp\htdocs\easyappointments\system\database\DB_driver.php 433
+ERROR - 2024-05-07 00:39:15 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\xampp\htdocs\easyappointments\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-05-07 00:39:15 --> Severity: error --> Exception: Unable to connect to the database. C:\xampp\htdocs\easyappointments\system\database\DB_driver.php 433
+ERROR - 2024-05-07 00:39:17 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\xampp\htdocs\easyappointments\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-05-07 00:39:17 --> Severity: error --> Exception: Unable to connect to the database. C:\xampp\htdocs\easyappointments\system\database\DB_driver.php 433
+ERROR - 2024-05-07 00:39:19 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\xampp\htdocs\easyappointments\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-05-07 00:39:19 --> Severity: error --> Exception: Unable to connect to the database. C:\xampp\htdocs\easyappointments\system\database\DB_driver.php 433
---
+++ storage/logs/log-2024-05-08.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-05-08 16:02:23 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\xampp\htdocs\easyappointments\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-05-08 16:02:23 --> Severity: error --> Exception: Unable to connect to the database. C:\xampp\htdocs\easyappointments\system\database\DB_driver.php 433
